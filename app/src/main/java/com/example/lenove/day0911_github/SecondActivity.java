4fd2d64a3cbe9e47687ff46b2d3887b925aed59d
11360500122 <2932905597@qq.com>
2017-09-11 17:28:04 +0800

    吐司操作

--- app/src/main/java/com/example/lenove/day0911_github/SecondActivity.java
+++ app/src/main/java/com/example/lenove/day0911_github/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.lenove.day0911_github;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends Activity {
 
@@ -9,5 +10,10 @@ public class SecondActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void click() {
+        Toast.makeText(this, "给我過一边", Toast.LENGTH_SHORT).show();
+
     }
 }
