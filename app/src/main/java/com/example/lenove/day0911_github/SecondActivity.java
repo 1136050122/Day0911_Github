8ee130851a10e0be44fca08c252f4797bb847b39
11360500122 <2932905597@qq.com>
2017-09-12 10:09:54 +0800

    输出日志操作

--- app/src/main/java/com/example/lenove/day0911_github/SecondActivity.java
+++ app/src/main/java/com/example/lenove/day0911_github/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.lenove.day0911_github;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class SecondActivity extends Activity {
@@ -13,6 +14,7 @@ public class SecondActivity extends Activity {
     }
 
     public void click() {
+        Log.e("TAG","可 能 了你这个人，真尼吗乃菜");
         Toast.makeText(this, "给我過一边", Toast.LENGTH_SHORT).show();
 
     }
